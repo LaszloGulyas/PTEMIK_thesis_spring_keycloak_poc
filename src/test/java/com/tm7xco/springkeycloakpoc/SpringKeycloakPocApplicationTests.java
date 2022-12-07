f2bb33f02a3fd4b017ff56c13d2a73596890cbf7
LaszloGulyas <laszlogulyas.jr@gmail.com>
2022-12-07 17:48:50 +0100

    init project with keycloak, postgres images

---
+++ src/test/java/com/tm7xco/springkeycloakpoc/SpringKeycloakPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm7xco.springkeycloakpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
