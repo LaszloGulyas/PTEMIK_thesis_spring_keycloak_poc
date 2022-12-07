5f3ab6238401ccd62b23b7c80d0106d50e727e0f
LaszloGulyas <laszlogulyas.jr@gmail.com>
2022-12-07 21:38:14 +0100

    fix annotation

--- src/main/java/com/tm7xco/springkeycloakpoc/config/SpringSecurityConfig.java
+++ src/main/java/com/tm7xco/springkeycloakpoc/config/SpringSecurityConfig.java
@@ -2,9 +2,9 @@ package com.tm7xco.springkeycloakpoc.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -12,7 +12,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-@EnableWebSecurity
+@Configuration
 public class SpringSecurityConfig {
 
     @Value("${cors-policies}")
