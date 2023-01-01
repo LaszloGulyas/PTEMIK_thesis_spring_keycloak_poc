16708e060065d3fee2feb303f816077aafc00533
LaszloGulyas <laszlogulyas.jr@gmail.com>
2023-01-01 21:34:00 +0100

    add angular dockerization with nginx

--- scripts/build-start-project.sh
+++ scripts/build-start-project.sh
@@ -3,6 +3,10 @@ cd "../backend/keycloak-rest-client-lib" || exit
 ./mvnw clean install -DskipTests
 cd "../spring-rest-server" || exit
 ./mvnw clean install -DskipTests
+cd "../../frontend/angular-nginx" || exit
+npm install
+npm run build --prod
 cd "../.."
 docker image rm spring-keycloak-poc-springapp
+docker image rm spring-keycloak-poc-angularapp
 docker-compose -f docker-compose.yml up -d
